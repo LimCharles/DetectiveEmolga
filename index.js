aa21efaf38ae007dd97605e344d44cc1e299a7bb
Charles <charles.andrew.lim@ateneo.obf.edu>
2020-12-29 22:55:45 +0800

    Fixed the egg shiny detector

--- index.js
+++ index.js
@@ -279,7 +279,7 @@ client.on('message', (message) => {
 			}
 		} //normal shiny spawn
 
-		if ((pokemon.includes('hatched an Egg!","icon"')) && (pokemon.includes('ani-shiny'))) {
+		if ((pokemon.includes('hatched an Egg!')) && (pokemon.includes('ani-shiny'))) {
 			pokembed.description = pokembed.description.replace(/<.*?>/g, ' ');
 			pokembed.addField('​​\u200b','This Pokémon was hatched in: ' + `[${message.channel.name}](${message.url})`);
 			for (let i = 0; i < channels.length; i++) {
